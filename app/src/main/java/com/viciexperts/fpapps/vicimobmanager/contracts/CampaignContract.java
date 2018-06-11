ca793ed28daa7bc116ac3fdc7b52607458042369
Fermin Perdomo <masterfermin02@gmail.com>
2018-06-11 17:59:10 -0400

    factory campaign activy

---
+++ app/src/main/java/com/viciexperts/fpapps/vicimobmanager/contracts/CampaignContract.java
@@ -0,0 +1,4 @@
+package com.viciexperts.fpapps.vicimobmanager.contracts;
+
+public interface CampaignContract {
+}
---
+++ app/src/main/java/com/viciexperts/fpapps/vicimobmanager/presentes/CampaignPresenter.java
@@ -0,0 +1,4 @@
+package com.viciexperts.fpapps.vicimobmanager.presentes;
+
+public class CampaignPresenter {
+}
